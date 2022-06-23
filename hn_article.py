8870b61d20a491d7afe2a79d756a4f70d3133b8f
SkymanSky <gokera@ogr.iu.edu.tr>
2022-06-23 22:37:08 +0300

    API çağrısı yapıldı veri yapısı incelenmesi için json dosyası olarak formatlı şeklilde dump edildi.

--- hn_article.py
+++ hn_article.py
@@ -5,8 +5,11 @@ import json
 url='https://hacker-news.firebaseio.com/v0/item/19155826.json'
 r=requests.get(url)
 print(f"Status code: {r.status_code}")
+
 #Verinin yapısını incele
 response_dict=r.json()
 readable_file='data/readable_hn_data.json'
+
+#Veriyi formatla dosyaya yaz.
 with open(readable_file,'w') as f:
     json.dump(response_dict,f,indent=4)
